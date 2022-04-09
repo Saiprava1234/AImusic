4939dd882755de8d5e9c272d8aa9a6f8c622150c
Saiprava1234 <86531553+Saiprava1234@users.noreply.github.com>
2022-04-09 21:38:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+HarryPotter = "";
+Believer = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+    HarryPotter = loadSound("HarryPotter.mp3");
+    Believer = loadSound("Believer.mp3");
+}
+function setup(){
+    canvas = createCanvas(370, 280);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet is Initialized !');
+}
+function draw(){
+    image(video, 0, 0, 380, 280);
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
